e7104d0367be6d6a45ec8742cad1a3cff5231c64
ByeTyme <123219254+ty1re@users.noreply.github.com>
2025-12-05 18:31:35 +0000

    pushing updated baby name file

--- Baby_Name_Filtering/Baby_Names.py
+++ Baby_Name_Filtering/Baby_Names.py
@@ -27,3 +27,36 @@ for file, year in filenames.items():
     
 names_df = pd.concat(all_years, ignore_index=True)
 print(names_df)
+
+
+# Create plot for total population
+count_total = []
+for year in range(1880, 2023):
+    temp = names_df[names_df['Year']==year]
+    count_total.append(temp['Count'].sum())
+
+plt.plot(names_df['Year'].unique(), count_total, color='orange', linewidth=2, markersize=1)
+plt.xlabel('Year')
+plt.ylabel('# of Babies')
+plt.title('Frequency of names from 1880-2022 (U.S.)')
+plt.savefig('Baby_Name_Frequency.png')
+plt.show()
+
+# Create plot for specific person
+Mary_df = names_df[names_df['Name']=='Mary']
+MaryF_df = Mary_df[Mary_df['Gender']=='F']
+print(MaryF_df)
+print(MaryF_df['Count'].max())
+max_mary = MaryF_df[MaryF_df['Count']==MaryF_df['Count'].max()]
+print(max_mary)
+plt.plot(MaryF_df['Year'], MaryF_df['Count'], color='orange', linewidth=2, markersize=1)
+plt.xlabel('Year')
+plt.ylabel('Babies named Mary')
+plt.title('Frequency of Mary from 1880-2022 (U.S.)')
+plt.savefig('Mary_Frequency.png')
+
+Mary_sum = Mary_df['Count'].sum()
+print("\n")
+print(f"Total babies names Mary: {Mary_sum}")
+
+plt.show()
